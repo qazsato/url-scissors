f91ee2b26f5586012932b44728f331ecfe7199af
qaz-s <kazuki-sato@kazuki-sato.local>
2015-06-16 21:56:10 +0900

    jQuery削除に伴う処理修正

--- index.js
+++ index.js
@@ -1,61 +1,58 @@
-$(function () {
-
-	chrome.tabs.getSelected(null, function(tab) {
-		var url = tab.url;
-		$("#urlarea").val(url);
-	});
-
-	$("#scissorsBtn").on("click", function () {
-		scissors($("#urlarea").val());
-		$("#result").show();
-	});
-
-	$("#deleteBtn").on("click", function () {
-		$("#urlarea").val("");
-	});
-
-	function scissors(value) {
-		var url = getUrl(value);
-		var path = url.split("?")[0];
-		var query = url.split("?")[1];
-
-		$("#path").text(decode(path));
-		$("tbody").empty();
-
-		if (query) {
-			var html = "";
-			var queryArray = query.split("&");
-			for (var i = 0; i < queryArray.length; i++) {
-				var q = queryArray[i].split("=");
-				html += getRowHtml(q[0], q[1]);
-			}
-			$("tbody").append(html);
-		}
-	}
-
-	function getRowHtml(key, value) {
-		return "<tr>" +
-					"<td>" + decode(key) + "</td>" +
-					"<td>" + decode(value) + "</td>" +
-				"</tr>";
-	}
-
-	function getUrl(value) {
-		if (value.substring(0, 4) === "http") {	// For URL
-			return value;
-		} else if (value.match("GET ") && value.match(" HTTP/")) {	// For AccessLog
-			return value.split("GET ")[1].split(" HTTP/")[0];
-		}
-		return value;
-	}
-
-	function decode(value) {
-		var str;
-		try {
-			str = decodeURIComponent(value);
-		} catch(e) {
-			str = value;
-		}
-		return str;
-	}
+chrome.tabs.getSelected(null, function(tab) {
+	var url = tab.url;
+	document.getElementById("urlarea").value = url;
 });
+
+document.getElementById("scissorsBtn").addEventListener("click", function() {
+	scissors(document.getElementById("urlarea").value);
+	document.getElementById("result").style.display = "block";
+});
+
+document.getElementById("deleteBtn").addEventListener("click", function() {
+	document.getElementById("urlarea").value = "";
+});
+
+function scissors(value) {
+	var url = getUrl(value);
+	var path = url.split("?")[0];
+	var query = url.split("?")[1];
+
+	document.getElementById("path").innerText = decode(path);
+	document.getElementsByTagName("tbody")[0].innerHTML = "";
+
+	if (query) {
+		var html = "";
+		var queryArray = query.split("&");
+		for (var i = 0; i < queryArray.length; i++) {
+			var q = queryArray[i].split("=");
+			html += getRowHtml(q[0], q[1]);
+		}
+		document.getElementsByTagName("tbody")[0].innerHTML = html;
+	}
+}
+
+function getRowHtml(key, value) {
+	return "<tr>" +
+				"<td>" + decode(key) + "</td>" +
+				"<td>" + decode(value) + "</td>" +
+			"</tr>";
+}
+
+function getUrl(value) {
+	if (value.substring(0, 4) === "http") {	// For URL
+		return value;
+	} else if (value.match("GET ") && value.match(" HTTP/")) {	// For AccessLog
+		return value.split("GET ")[1].split(" HTTP/")[0];
+	}
+	return value;
+}
+
+function decode(value) {
+	var str;
+	try {
+		str = decodeURIComponent(value);
+	} catch(e) {
+		str = value;
+	}
+	return str;
+}
