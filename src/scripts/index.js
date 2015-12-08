a7f498038d6ee2a58b13c4d8dcc58bbccd4a1d23
qaz.s <qaz.s.co@gmail.com>
2015-12-09 01:33:00 +0900

    ES6の記法に変更

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,56 +1,58 @@
-chrome.tabs.getSelected(null, function(tab) {
-  var url = tab.url;
-  document.getElementById("urlarea").value = url;
+'use strict';
+
+chrome.tabs.getSelected(null, tab => {
+  let url = tab.url;
+  document.getElementById('urlarea').value = url;
   scissors(url);
-  document.getElementById("result").style.display = "block";
 });
 
-document.getElementById("scissorsBtn").addEventListener("click", function() {
-  scissors(document.getElementById("urlarea").value);
-  document.getElementById("result").style.display = "block";
+document.getElementById('scissors-btn').addEventListener('click', () => {
+  scissors(document.getElementById('urlarea').value);
 });
 
-document.getElementById("deleteBtn").addEventListener("click", function() {
-  document.getElementById("urlarea").value = "";
+document.getElementById('delete-btn').addEventListener('click', () => {
+  document.getElementById('urlarea').value = null;
+  document.getElementById('urlbox').classList.remove('is-dirty');
 });
 
-function scissors(value) {
-  var url = getUrl(value);
-  var path = url.split("?")[0];
-  var query = url.split("?")[1];
+let scissors = value => {
+  let url = getUrl(value);
+  let path = url.split('?')[0];
+  let query = url.split('?')[1];
 
-  document.getElementById("path").innerText = decode(path);
-  document.getElementsByTagName("tbody")[0].innerHTML = "";
+  document.getElementById('path').innerText = decode(path);
+  document.getElementsByTagName('tbody')[0].innerHTML = '';
 
   if (query) {
-    var html = "";
-    var queryArray = query.split("&");
-    for (var i = 0; i < queryArray.length; i++) {
-      var q = queryArray[i].split("=");
+    let html = '';
+    let queryArray = query.split('&');
+    for (let i = 0; i < queryArray.length; i++) {
+      let q = queryArray[i].split('=');
       html += getRowHtml(q[0], q[1]);
     }
-    document.getElementsByTagName("tbody")[0].innerHTML = html;
+    document.getElementsByTagName('tbody')[0].innerHTML = html;
+    document.getElementById('result').style.display = 'block';
   }
 }
 
-function getRowHtml(key, value) {
-  return '<tr>' +
-        '<td class="mdl-data-table__cell--non-numeric">' + decode(key) + '</td>' +
-        '<td class="mdl-data-table__cell--non-numeric">' + decode(value) + '</td>' +
-      '</tr>';
+let getRowHtml = (key, value) => {
+  return `<tr>
+            <td class="mdl-data-table__cell--non-numeric">${decode(key)}</td>
+            <td class="mdl-data-table__cell--non-numeric">${decode(value)}</td>
+          </tr>`;
 }
 
-function getUrl(value) {
-  if (value.substring(0, 4) === "http") {  // For URL
+let getUrl = value => {
+  if (value.substring(0, 4) === 'http') {  // For URL
     return value;
-  } else if (value.match("GET ") && value.match(" HTTP/")) {  // For AccessLog
-    return value.split("GET ")[1].split(" HTTP/")[0];
+  } else if (value.match('GET ') && value.match(' HTTP/')) {  // For AccessLog
+    return value.split('GET ')[1].split(' HTTP/')[0];
   }
   return value;
 }
 
-function decode(value) {
-  var str;
+let decode = value => {
+  let str;
   try {
     str = decodeURIComponent(value);
   } catch(e) {
